326b0878fd3fdbad413a2386a1184c9a7de48884
Leonardino80 <gilgamesh2005@gmail.com>
2019-04-25 08:19:50 +0200

    finito while

--- main.js
+++ main.js
@@ -2,14 +2,43 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.”
 
-var i = 0
-while (i < 100) {
-document.getElementById('fizzy').innerHTML = i;
+var i = 1;
+while (i <= 100)
+{
+  if (i % 3 == 0 && i % 5 == 0)
+  {
+    document.getElementById('fizzy').innerHTML += 'fizzbuzz' + '<br>';
+  }
+  else if (i % 5 == 0)
+  {
+    document.getElementById('fizzy').innerHTML += 'buzz' + '<br>';
+  }
+  else if (i % 3 == 0)
+  {
+    document.getElementById('fizzy').innerHTML += 'fizz' + '<br>';
+  }
+  else {
+    document.getElementById('fizzy').innerHTML += i + '<br>';
+  }
   i++;
 }
 
-var y = 0
-while (y < 100) {
-document.write (y);
-  y++;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// var y = 0;
+// while (y < 100) {
+// document.write (y);
+//   y++;
+// }
